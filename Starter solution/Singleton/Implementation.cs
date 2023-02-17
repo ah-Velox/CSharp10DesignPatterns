90b63ca7afbc8f9752e050e2000faf0151b183ea
Achal Hingrajiya <Achal.Hingrajiya@veloxcore.com>
2023-02-17 12:54:31 +0530

    Implemented Singleton pattern

--- Starter solution/Singleton/Implementation.cs
+++ Starter solution/Singleton/Implementation.cs
@@ -1,4 +1,27 @@
 ﻿namespace Singleton
 {
-  
+    // Singleton
+    class Logger
+    {
+        private static Logger? _instance;
+        private Logger() { }
+
+        // Singleton Instance
+        public static Logger? Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+
+        // Singleton Operation
+        public void Log(string message) { 
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
 }
--- Starter solution/Singleton/Program.cs
+++ Starter solution/Singleton/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.Title = "Singleton";
+﻿using Singleton;
+
+Console.Title = "Singleton";
+
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if(instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("All instances are same");
+}
+
+Console.ReadLine();
